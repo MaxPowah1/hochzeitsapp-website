13ca513f1dad542da22ca176195ab5a6d8037d32
jrraid <max.scherb@googlemail.com>
2025-02-26 19:03:06 +0100

    Add store to database 8

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Connect to MongoDB
 // Replace 'your-database-name' with your actual database name or use an environment variable.
-mongoose.connect('mongodb://localhost:27017/your-database-name');
+mongoose.connect('mongodb://localhost:27017/orders');
 mongoose.connection.on('error', err => {
   console.error('MongoDB connection error:', err);
 });
