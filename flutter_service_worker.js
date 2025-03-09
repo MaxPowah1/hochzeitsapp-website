beaa84071b814cfc9acc320deef9a56d3392cdd7
jrraid <max.scherb@googlemail.com>
2025-03-09 20:40:50 +0100

    Gifts 4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -56,11 +56,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "c7a407662fe7b439bf8e52f107735e96
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "editor.html": "a9663682d2c6e73d4bcd0418014b3d9f",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "6f8d7a7435f07514efab22e29691b362",
+"flutter_bootstrap.js": "f7220bfbe7811442841cf765c3587eda",
 "Icon2_512x512.png": "716e9d5eb646052dff2a73633c4edcbc",
 "index.html": "a9663682d2c6e73d4bcd0418014b3d9f",
 "/": "a9663682d2c6e73d4bcd0418014b3d9f",
-"main.dart.js": "7ef3a606f3bcc39315738903ffbbc23d",
+"main.dart.js": "b65778cab7a4ebf63e46a222d6aff0a8",
 "version.json": "b3cba2a71364b234158409f8c335c0f4"};
 // The application shell files that are downloaded before a service worker can
 // start.
