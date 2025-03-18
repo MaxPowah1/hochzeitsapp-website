5779465c656a1d4d0621044e0995d68cc6824a78
jrraid <max.scherb@googlemail.com>
2025-03-18 18:05:48 +0100

    Reducing repos 2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "284622914"
+    serviceWorkerVersion: "3869523247"
   }
 });
